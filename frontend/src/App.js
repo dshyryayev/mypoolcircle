f1a64c02d435ead2dc8de73556196ec32e35ed9c
dshyryayev <dshyryayev@gmail.com>
2024-08-22 17:52:32 -0700

    set is memeber finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavbarMenu from "./components/Navbar";
 import Register from "./components/Register";
 import Pools from "./components/Pools";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { connect, getContractAndSigner } from "./pool_circle_contract";
 
 function App() {
@@ -11,6 +11,25 @@ function App() {
   const [contract, setContract] = useState(null);
   const [connected, setConnected] = useState(false);
   const [isMember, setIsMember] = useState(false);
+
+  const handleMemberState = useCallback(async (signer, contract) => {
+    const signerAddress = await signer.getAddress();
+    console.log({ address: signerAddress });
+    contract.members(signerAddress).then((resultIsMember) => {
+      console.log({ resultIsMember });
+      setIsMember(resultIsMember);
+    });
+  }, []);
+
+  const handleInit = useCallback(async () => {
+    setConnected(true);
+    const { signer, contract } = await getContractAndSigner();
+    setContract(contract);
+
+    if (contract) {
+      await handleMemberState(signer, contract);
+    }
+  }, [handleMemberState]);
 
   useEffect(() => {
     window.ethereum.request({ method: 'eth_accounts' })
@@ -21,36 +40,15 @@ function App() {
           setConnected(false)
         };
       })
-  }, []);
-
-  const handleInit = async () => {
-    setConnected(true);
-    const { signer, contract } = await getContractAndSigner();
-    setContract(contract);
-
-    if (contract) {
-      signer.getAddress().then((address) => {
-        console.log({ address });
-        contract.members(address).then((resultIsMember) => {
-          console.log({ resultIsMember });
-          setIsMember(resultIsMember);
-        });
-      });
-    }
-  }
+  }, [handleInit]);
 
   const connectCallback = async () => {
     const { signer, contract } = await connect();
     setContract(contract);
     if (contract) {
       setConnected(true);
-      signer.getAddress().then((address) => {
-        console.log({ address });
-        contract.members(address).then((resultIsMember) => {
-          console.log({ resultIsMember });
-          setIsMember(resultIsMember);
-        });
-      });
+      await handleMemberState(signer, contract);
+      ;
     } else {
       setConnected(false);
     }
